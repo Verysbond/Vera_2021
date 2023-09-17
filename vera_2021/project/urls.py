4b80016a01b0cb6aa518597edb26b0b203cd184f
Admin <verysbond@ya.ru>
2023-09-17 17:46:32 +0300

    start_files

--- vera_2021/project/urls.py
+++ vera_2021/project/urls.py
@@ -1,7 +1,10 @@
 from django.contrib import admin
+
 from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('project/', views.project),
+    path("", views.project_index, name="blog_index"),
+    path("pr", views.project_index, name="project_index"),
+    path("<int:pk>/", views.project_detail, name="project_detail"),
 ]
