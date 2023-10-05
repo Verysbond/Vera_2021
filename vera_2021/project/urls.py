f10c62f9bf4391c03344a5a660e372d57351c868
Admin <verysbond@ya.ru>
2023-10-05 16:06:51 +0300

    start_files

--- vera_2021/project/urls.py
+++ vera_2021/project/urls.py
@@ -4,7 +4,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.project_index, name="blog_index"),
-    path("pr", views.project_index, name="project_index"),
+    path("", views.project_index, name="project_index"),
     path("<int:pk>/", views.project_detail, name="project_detail"),
 ]
